dda3f6775a5096f0f23bdfafa6778fa922dc98b3
YasinDoyle <yasindoyle97@gmail.com>
2023-01-20 23:14:17 +0800

    initial commit

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,20 +1,37 @@
-const { network } = require("hardhat")
+//main function
+//calling of main function
+
+// async function deployFunc(hre) {
+//     console.log("hi")
+// }
+
+// module.exports.default = deployFunc
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
 const { verify } = require("../utils/verify")
 require("dotenv").config()
+//以上这行的操作等同于
+// const helperConfig = require("../helper-hardhat-config")
+// const networkConfig = helperConfig.networkConfig
 
+//另一种写法
+//getNamedAccounts = hre.getNamedAccounts, deployments = hre.deployments
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
+    //const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     let ethUsdPriceFeedAddress
     if (chainId == 31337) {
+        //mock的话用mock的地址，其他的去chainlink网站查api地址
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
+
+    //deploy 这个合约
     log("----------------------------------------------------")
     log("Deploying FundMe and waiting for confirmations...")
     const fundMe = await deploy("FundMe", {
@@ -22,7 +39,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         args: [ethUsdPriceFeedAddress],
         log: true,
         // we need to wait if on a live network so we can verify properly
-        waitConfirmations: network.config.blockConfirmations || 1,
+        waitConfirmations: network.config.blockConfirmations || 1
     })
     log(`FundMe deployed at ${fundMe.address}`)
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -23,43 +23,47 @@ module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            chainId: 31337,
+            chainId: 31337
             // gasPrice: 130000000000,
         },
         goerli: {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 5,
-            blockConfirmations: 6,
-        },
+            blockConfirmations: 6 //愿意等多少个block
+        }
     },
     solidity: {
         compilers: [
             {
-                version: "0.8.7",
+                version: "0.8.7"
             },
             {
-                version: "0.6.6",
-            },
-        ],
+                version: "0.6.6"
+            }
+        ]
     },
     etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY
     },
     gasReporter: {
         enabled: true,
         currency: "USD",
         outputFile: "gas-report.txt",
-        noColors: true,
+        noColors: true
         // coinmarketcap: COINMARKETCAP_API_KEY,
     },
     namedAccounts: {
         deployer: {
+            //对不同的net来说，可能存在很多不同的account,如account[privatekey1,key2,key3]
+            //这些accounts下标从0开始，可以指定不同网络的account下标
+            //1：0代表主网默认是第一个下标对应的account
+            //5:1代表goerli默认的是第二个下标对应的account
             default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
+            1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        }
     },
     mocha: {
-        timeout: 500000,
-    },
+        timeout: 500000
+    }
 }
